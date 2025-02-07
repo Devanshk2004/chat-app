7ca67aa80fd76932edd8c9a0cecb368b6c1769ff
Devanshk2004 <134846636+Devanshk2004@users.noreply.github.com>
2025-02-07 22:19:18 +0530

    cy

--- frontend/pages/_app.jsx
+++ frontend/pages/_app.jsx
@@ -1,24 +1,29 @@
 import '../styles/globals.css';
 import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 import Navbar from '@/components/navbar';
 import { useAuthStore } from '@/store/useAuth';
 import { useThemeStore } from '@/store/useTheme';
 import { Loader } from 'lucide-react';
 import toast, { Toaster } from 'react-hot-toast';
 
-
-
-
-
 const App = ({ Component, pageProps }) => {
   const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
   const { theme } = useThemeStore();
+  const router = useRouter();
+
 
   useEffect(() => {
     checkAuth();
-  }, [checkAuth]);
+  }, []);
 
-  if (isCheckingAuth && !authUser) {
+  useEffect(() => {
+    if (!isCheckingAuth && !authUser && router.pathname !== '/login') {
+      router.push('/login');
+    }
+  }, [isCheckingAuth, authUser, router.pathname]);
+
+  if (isCheckingAuth) {
     return (
       <div className="flex items-center justify-center h-screen">
         <Loader className="size-10 animate-spin" />
@@ -28,7 +33,7 @@ const App = ({ Component, pageProps }) => {
 
   return (
     <div data-theme={theme}>
-      <Navbar />
+      {authUser && <Navbar />}
       <Component {...pageProps} />
       <Toaster />
     </div>
@@ -36,3 +41,6 @@ const App = ({ Component, pageProps }) => {
 };
 
 export default App;
+
+
+
